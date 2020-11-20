3238917c8da2fe4e6ac67d4a4e58625f2155375e
Rodrigo Lima <drigo.lima1987@gmail.com>
2020-11-19 21:48:30 -0300

    Iniciando implementacao do tabuleiro e peças

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,12 @@
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-		Posicao pos = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 		
-		
-		System.out.println(pos);
 	}
 
 }
